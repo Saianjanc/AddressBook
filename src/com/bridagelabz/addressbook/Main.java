0af22a34353b7075693615ba50b1917ea6ae925d
unknown <saianjan1234@gmail.com>
2023-11-27 13:24:43 +0530

    Added Menu based options for displayin contacts.

--- src/com/bridagelabz/addressbook/Main.java
+++ src/com/bridagelabz/addressbook/Main.java
@@ -14,17 +14,33 @@ public class Main {
         System.out.println("Welcome to AddressBook");
         do {
             System.out.println(menu);
-            CrudOperation c1 = new CrudOperation();
             input = sc.nextInt();
 
             switch (input) {
                 //adding the contact in The current address book instance using addContact method
-                case 1 -> c1.AddContact(a1);
+                case 1 -> AddContact(a1);
                 //printing the current address book contact list
                 case 2 -> System.out.println(a1.contactList);
                 case 0 -> System.out.println("Bye bye!!");
                 default -> System.out.println("Enter valid option");
             }
         }while (input!=0);
+    }
+    private static void AddContact(AddressBook a) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first name");
+        String fname = sc.next();
+        System.out.println("Enter last name");
+        String lname = sc.next();
+        System.out.println("Enter Phone number");
+        String phoneNumber = sc.next();
+        System.out.println("Enter ZipCode");
+        int zip = sc.nextInt();
+        System.out.println("Enter Relation");
+        String relation = sc.next();
+        //person constructor
+        Person p1 = new Person(fname,lname,phoneNumber,zip,relation);
+        //added to the address book instance
+        a.contactList.add(p1);
     }
 }
