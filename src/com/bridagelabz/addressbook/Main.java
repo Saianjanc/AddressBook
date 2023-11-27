0e8ef64fb29fea1c9a77e5b003ab9f788a430f7a
unknown <saianjan1234@gmail.com>
2023-11-27 14:23:23 +0530

    Added Option for Multiple Address Books!

--- src/com/bridagelabz/addressbook/Main.java
+++ src/com/bridagelabz/addressbook/Main.java
@@ -1,17 +1,29 @@
 package com.bridagelabz.addressbook;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         AddressBook a1 = new AddressBook("Book1");
-
+        AddressBook a2 = new AddressBook("Book2");
+        AddressBook a3 = new AddressBook("Book3");
+        AddressBook a4 = new AddressBook("Book4");
+        AddressBook a5 = new AddressBook("Book5");
+        ArrayList<AddressBook> AddressBooks = new ArrayList<>();
+        AddressBooks.add(a1);
+        AddressBooks.add(a2);
+        AddressBooks.add(a3);
+        AddressBooks.add(a4);
+        AddressBooks.add(a5);
         Scanner sc = new Scanner(System.in);
-        String menu = "Press \n1: Adding a contact\n2: Displaying All Contacts\n3: Search by first name\n4: Search by Phone number\n5: Delete by User First name\n6: Update Phone Number\n0: To Exit the Program.";
+        String menu = "Press \n1: Adding a contact\n2: Displaying All Contacts\n3: Search by first name\n4: Search by Phone number\n5: Delete by User First name\n6: Update Phone Number\n7: Change AddressBook\n0: To Exit the Program.";
 
         //Menu based programming
-        int input;
-        System.out.println("Welcome to AddressBook");
+        int bookno,input;
+        System.out.println("Welcome to com.bridagelabz.addressbook.AddressBook");
+        System.out.println("Enter Book Number:");
+        bookno=sc.nextInt();
         do {
             System.out.println(menu);
             CrudOperations c1 = new CrudOperations();
@@ -19,29 +31,33 @@ public class Main {
 
             switch (input) {
                 //adding the contact in The current address book instance using addContact method
-                case 1 -> c1.AddContact(a1);
+                case 1 -> c1.AddContact(AddressBooks.get(bookno));
                 //printing the current address book contact list
-                case 2 -> System.out.println(a1.contactList);
+                case 2 -> System.out.println(AddressBooks.get(bookno).contactList);
                 //Searching a name in the addressbook
                 case 3 -> {
                     System.out.println("Enter the name you want to search");
                     String s1 = sc.next();
-                    c1.searchByFirstName(a1,s1);
+                    c1.searchByFirstName(AddressBooks.get(bookno),s1);
                 }
                 case 4 -> {
                     System.out.println("Enter the Phone Number you want to search");
                     String s1 = sc.next();
-                    c1.searchByPhoneNumber(a1,s1);
+                    c1.searchByPhoneNumber(AddressBooks.get(bookno),s1);
                 }
                 case 5 -> {
                     System.out.println("Enter the Name you want to delete");
                     String s1 = sc.next();
-                    c1.deleteContactByfName(a1,s1);
+                    c1.deleteContactByfName(AddressBooks.get(bookno),s1);
                 }
                 case 6 -> {
                     System.out.println("Enter the Name you want to update the phone number");
                     String s1 = sc.next();
-                    c1.updatePhoneNumberByName(a1,s1);
+                    c1.updatePhoneNumberByName(AddressBooks.get(bookno),s1);
+                }
+                case 7 -> {
+                    System.out.println("Enter Book Number:");
+                    bookno=sc.nextInt();
                 }
                 case 0 -> System.out.println("Bye bye!!");
                 default -> System.out.println("Enter valid option");
